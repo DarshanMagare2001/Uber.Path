1761264fdcb12d698f7a5468293b1c7b8fc22f7a
Darshan <>
2023-06-23 14:31:27 +0530

    new changes

--- UberPath/UberPath/Controller/OnboardingVC.swift
+++ UberPath/UberPath/Controller/OnboardingVC.swift
@@ -13,38 +13,34 @@ class OnboardingVC: UIViewController {
     @IBOutlet weak var onboardingTwoView: UIView!
     @IBOutlet weak var onboardOneViewLbl1: UILabel!
     @IBOutlet weak var onboardOneViewLbl2: UILabel!
+    @IBOutlet weak var onboardTwoViewLbl1: UILabel!
+    @IBOutlet weak var onboardTwoViewLbl2: UILabel!
     
-    
+    var viewModel : FontManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         onboardingOneView.isHidden = false
         onboardingTwoView.isHidden = true
-
-        let baseFontSize: CGFloat = 17.0
-        let adjustedFontSize = FontManager.adjustedFontSize(forBaseSize: baseFontSize)
-        onboardOneViewLbl1.font = UIFont.systemFont(ofSize: adjustedFontSize)
-        
-        
-        
+        updateFont()
         
     }
-    
     
     @IBAction func onboardingOneViewNxtBtnPressed(_ sender: UIButton) {
         onboardingTwoView.isHidden = false
         onboardingOneView.isHidden = true
-        
     }
-    
-    
     
     @IBAction func onboardingTwoViewGetstartedBtnPressed(_ sender: UIButton) {
-        
-        
-        
+        // Handle get started button press
     }
     
-
+    func updateFont(){
+        onboardOneViewLbl1.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        onboardOneViewLbl2.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 6.0))
+        onboardTwoViewLbl1.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        onboardTwoViewLbl2.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 6.0))
+    }
+    
 }
 
--- UberPath/UberPath/Extensions/Fonts.swift
+++ UberPath/UberPath/Extensions/Fonts.swift
@@ -2,6 +2,10 @@ import UIKit
 
 extension UIDevice {
     var modelName: String {
+        if let simulatorModelIdentifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] {
+            return simulatorModelIdentifier
+        }
+        
         var systemInfo = utsname()
         uname(&systemInfo)
         let machineMirror = Mirror(reflecting: systemInfo.machine)
@@ -9,36 +13,101 @@ extension UIDevice {
             guard let value = element.value as? Int8, value != 0 else { return identifier }
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
-        return identifier
+        
+        // Map the identifier to the corresponding model name
+        let modelMapping: [String: String] = [
+            // iPhone
+            "iPhone1,1": "iPhone 1G",
+            "iPhone1,2": "iPhone 3G",
+            "iPhone2,1": "iPhone 3GS",
+            "iPhone3,1": "iPhone 4",
+            "iPhone3,2": "iPhone 4",
+            "iPhone3,3": "iPhone 4",
+            "iPhone4,1": "iPhone 4S",
+            "iPhone5,1": "iPhone 5",
+            "iPhone5,2": "iPhone 5",
+            "iPhone5,3": "iPhone 5C",
+            "iPhone5,4": "iPhone 5C",
+            "iPhone6,1": "iPhone 5S",
+            "iPhone6,2": "iPhone 5S",
+            "iPhone7,1": "iPhone 6 Plus",
+            "iPhone7,2": "iPhone 6",
+            "iPhone8,1": "iPhone 6S",
+            "iPhone8,2": "iPhone 6S Plus",
+            "iPhone8,4": "iPhone SE (1st generation)",
+            "iPhone9,1": "iPhone 7",
+            "iPhone9,2": "iPhone 7 Plus",
+            "iPhone9,3": "iPhone 7",
+            "iPhone9,4": "iPhone 7 Plus",
+            "iPhone10,1": "iPhone 8",
+            "iPhone10,2": "iPhone 8 Plus",
+            "iPhone10,3": "iPhone X",
+            "iPhone10,4": "iPhone 8",
+            "iPhone10,5": "iPhone 8 Plus",
+            "iPhone10,6": "iPhone X",
+            "iPhone11,2": "iPhone XS",
+            "iPhone11,4": "iPhone XS Max",
+            "iPhone11,6": "iPhone XS Max",
+            "iPhone11,8": "iPhone XR",
+            "iPhone12,1": "iPhone 11",
+            "iPhone12,3": "iPhone 11 Pro",
+            "iPhone12,5": "iPhone 11 Pro Max",
+            "iPhone12,8": "iPhone SE (2nd generation)",
+            "iPhone13,1": "iPhone 12 mini",
+            "iPhone13,2": "iPhone 12",
+            "iPhone13,3": "iPhone 12 Pro",
+            "iPhone13,4": "iPhone 12 Pro Max",
+            "iPhone14,4": "iPhone 13 mini",
+            "iPhone14,5": "iPhone 13",
+            "iPhone14,2": "iPhone 13 Pro",
+            "iPhone14,3": "iPhone 13 Pro Max",
+            "iPhone14,8": "iPhone 13 Pro Max",
+
+            // Add more mappings for other iPhone models if needed
+        ]
+
+        if let modelName = modelMapping[identifier] {
+            print(identifier)
+            return modelName
+        } else {
+            return identifier
+        }
     }
 }
 
 class FontManager {
-    
-    static func scaledFont(forTextStyle textStyle: UIFont.TextStyle, compatibleWith traitCollection: UITraitCollection) -> UIFont {
-        let font = UIFont.preferredFont(forTextStyle: textStyle)
-        let fontMetrics = UIFontMetrics(forTextStyle: textStyle)
-        return fontMetrics.scaledFont(for: font, compatibleWith: traitCollection)
-    }
-    
     static func adjustedFontSize(forBaseSize baseSize: CGFloat) -> CGFloat {
         let device = UIDevice.current
-        let modelName = device.modelName
+        let modelName = device.name
+        print(modelName)
+        var adjustedSize = baseSize // Default size
         
         switch modelName {
-        case "iPhone SE", "iPhone SE (2nd generation)", "iPhone 8":
-            return baseSize * 0.8 // Adjust font size for smaller iPhones
-        case "iPhone 8 Plus", "iPhone X", "iPhone XS", "iPhone XR", "iPhone 11", "iPhone SE (2nd generation)",
-             "iPhone 11 Pro", "iPhone 12 mini":
-            return baseSize * 0.9 // Adjust font size for medium-sized iPhones
-        case "iPhone 11 Pro Max", "iPhone XS Max", "iPhone XR (China)", "iPhone 12", "iPhone 12 Pro", "iPhone 13 mini":
-            return baseSize * 1.0 // Adjust font size for larger iPhones
-        case "iPhone 12 Pro Max", "iPhone 13", "iPhone 13 Pro", "iPhone 13 Pro Max":
-            return baseSize * 1.1 // Adjust font size for largest iPhones
+        case "iPhone 1G", "iPhone 3G", "iPhone 3GS":
+            adjustedSize *= 0.8
+        case "iPhone 4", "iPhone 4S":
+            adjustedSize *= 0.9
+            
+        case "iPhone 5", "iPhone 5C", "iPhone 5S","iPhone SE (1st generation)" :
+            adjustedSize *= 1
+            
+        case "iPhone 6", "iPhone 6S", "iPhone 7", "iPhone 8" , "iPhone SE (2nd generation)":
+            adjustedSize *= 1.4
+            
+        case "iPhone 6 Plus", "iPhone 6S Plus", "iPhone 7 Plus", "iPhone 8 Plus", "iPhone X", "iPhone XR":
+            adjustedSize *= 1.8
+        case "iPhone XS", "iPhone 11":
+            adjustedSize *= 2
+        case "iPhone XS Max", "iPhone 11 Pro", "iPhone 11 Pro Max", "iPhone 12 mini", "iPhone 12", "iPhone 12 Pro", "iPhone 12 Pro Max", "iPhone 13 Pro", "iPhone 13 Pro Max", "iPhone 13 mini", "iPhone 13", "iPhone 13 Pro Max":
+            adjustedSize *= 2.2
         default:
-            return baseSize // Use the base size for other iPhones
+            break // No adjustment needed
         }
+        
+        print("Adjusted font size for \(modelName): \(adjustedSize)")
+        return adjustedSize
     }
-    
 }
+
+
 
