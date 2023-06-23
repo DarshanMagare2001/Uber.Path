13842b09627ee1d4e99f6098974309604ef4afdb
Darshan <>
2023-06-23 17:36:29 +0530

    new changes

--- UberPath/UberPath/Controller/AuthenticationVC.swift
+++ UberPath/UberPath/Controller/AuthenticationVC.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class AuthenticationVC: UIViewController {
+class AuthenticationVC: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var hiThereLbl: UILabel!
     @IBOutlet weak var welcomeLbl: UILabel!
+    @IBOutlet weak var emailTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateFont()
+        emailTxtFld.delegate = self
+        passwordTxtFld.delegate = self
     }
     
     @IBAction func backArrowBtnPressed(_ sender: UIButton) {
@@ -24,6 +29,47 @@ class AuthenticationVC: UIViewController {
     func updateFont(){
         hiThereLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 18.0))
         welcomeLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 10.0))
+    }
+    
+    @IBAction func passwordHideShowBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func forgotPasswordBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func signInBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func signInWithGoogleBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func signInWithAppleBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func signUpBtnPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Dismiss the keyboard when the return button is tapped
+        textField.resignFirstResponder()
+        return true
     }
     
     
