8836da6f71574663fd3848de4e0dd93a621334df
Darshan <>
2023-06-28 18:15:17 +0530

    new changes

--- UberPath/UberPath/Controller/Onboarding/ProofOfResidencyVC.swift
+++ UberPath/UberPath/Controller/Onboarding/ProofOfResidencyVC.swift
@@ -8,21 +8,26 @@
 import UIKit
 
 class ProofOfResidencyVC: UIViewController {
-    
+    @IBOutlet weak var proofLbl: UILabel!
+    @IBOutlet weak var proveLbl: UILabel!
+    @IBOutlet weak var nationalityLbl: UILabel!
+    @IBOutlet weak var methodsLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateFont()
         
     }
-    
-    
-    
-    
+     
     @IBAction func backBtnPressed(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
     
-    
+    func updateFont() {
+        proofLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 18.0))
+        proveLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 15.0))
+        nationalityLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 15.0))
+        methodsLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 15.0))
+    }
     
 }
