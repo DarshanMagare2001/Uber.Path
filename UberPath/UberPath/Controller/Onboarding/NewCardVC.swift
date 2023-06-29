31bbd88c963973f471788935367a24eb106b2c09
Darshan <>
2023-06-29 18:23:56 +0530

    new changes

--- UberPath/UberPath/Controller/Onboarding/NewCardVC.swift
+++ UberPath/UberPath/Controller/Onboarding/NewCardVC.swift
@@ -1,14 +1,8 @@
-//
-//  NewCardVC.swift
-//  UberPath
-//
-//  Created by IPS-161 on 29/06/23.
-//
-
 import UIKit
 
-class NewCardVC: UIViewController , UITextFieldDelegate {
-    var selectedImageName : String?
+class NewCardVC: UIViewController, UITextFieldDelegate {
+    var selectedImageName: String?
+    
     @IBOutlet weak var cardDetailLbl: UILabel!
     @IBOutlet weak var cardImageView: UIImageView!
     @IBOutlet weak var cardNumberTxtFld: UITextField!
@@ -17,10 +11,10 @@ class NewCardVC: UIViewController , UITextFieldDelegate {
     @IBOutlet weak var cardHolderTxtFld: UITextField!
     @IBOutlet weak var countryImageView: UIImageView!
     @IBOutlet weak var countryLbl: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        cardImageView.image = UIImage(named: selectedImageName ?? "")
         updateFont()
         updatePlaceHolders()
         cardNumberTxtFld.delegate = self
@@ -28,23 +22,20 @@ class NewCardVC: UIViewController , UITextFieldDelegate {
         vccTxtFld.delegate = self
         cardHolderTxtFld.delegate = self
         
+        registerForKeyboardNotifications()
     }
     
+    deinit {
+        unregisterFromKeyboardNotifications()
+    }
     
     @IBAction func countryShowBtnPressed(_ sender: UIButton) {
-        
-        
-        
+        // Handle country button action
     }
-    
-    
     
     @IBAction func saveBtnPressed(_ sender: UIButton) {
-        
-        
+        // Handle save button action
     }
-    
-    
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
@@ -54,7 +45,7 @@ class NewCardVC: UIViewController , UITextFieldDelegate {
         cardDetailLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 20.0))
     }
     
-    func updatePlaceHolders(){
+    func updatePlaceHolders() {
         cardNumberTxtFld.placeholder = "Card number"
         expiryDateTxtFld.placeholder = "Expiry date"
         vccTxtFld.placeholder = "VCC"
@@ -62,9 +53,57 @@ class NewCardVC: UIViewController , UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // Dismiss the keyboard when the return button is tapped
         textField.resignFirstResponder()
         return true
     }
     
+    // MARK: - Keyboard Handling
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        // Calculate the height of the keyboard
+        let keyboardHeight = keyboardFrame.height
+        
+        // Adjust the scroll view content insets to avoid the keyboard
+        scrollView.contentInset.bottom = keyboardHeight
+        scrollView.scrollIndicatorInsets.bottom = keyboardHeight
+        
+        // Scroll the active text field into view
+        if let activeTextField = findActiveTextField() {
+            let visibleContentHeight = scrollView.bounds.height - keyboardHeight
+            
+            // Check if the active text field is hidden by the keyboard
+            if !(0 ..< visibleContentHeight).contains(activeTextField.frame.maxY) {
+                let scrollOffset = CGPoint(x: 0, y: activeTextField.frame.maxY - visibleContentHeight)
+                scrollView.setContentOffset(scrollOffset, animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        // Reset the scroll view content insets
+        scrollView.contentInset = .zero
+        scrollView.scrollIndicatorInsets = .zero
+    }
+    
+    func findActiveTextField() -> UITextField? {
+        // Find the active text field among the defined text fields
+        let textFields: [UITextField] = [cardNumberTxtFld, expiryDateTxtFld, vccTxtFld, cardHolderTxtFld]
+        return textFields.first { $0.isFirstResponder }
+    }
 }
+
