cf04a9bc4b8bcaac7a29881b07d827d335af64c8
Darshan <>
2023-06-29 14:04:20 +0530

    new changes

---
+++ UberPath/UberPath/Controller/Onboarding/CardCell.swift
@@ -0,0 +1,26 @@
+//
+//  CardCell.swift
+//  UberPath
+//
+//  Created by IPS-161 on 29/06/23.
+//
+
+import UIKit
+
+class CardCell: UITableViewCell {
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+        
+    }
+    
+}
--- UberPath/UberPath/Controller/Onboarding/CreateCardVC.swift
+++ UberPath/UberPath/Controller/Onboarding/CreateCardVC.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 
-class CreateCardVC: UIViewController {
+class CreateCardVC: UIViewController , UITableViewDelegate,UITableViewDataSource {
+    
+    
     @IBOutlet weak var createLbl: UILabel!
     @IBOutlet weak var theCustomizeLbl: UILabel!
+    @IBOutlet weak var cardTableViewOutlet: UITableView!
+    var cards :[String] = ["CardStyleOne","CardStyleTwo","CardStyleThree"]
     override func viewDidLoad() {
         super.viewDidLoad()
         updateFont()
+        cardTableViewOutlet.delegate = self
+        cardTableViewOutlet.dataSource = self
         
     }
     
@@ -25,5 +31,18 @@ class CreateCardVC: UIViewController {
         createLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 21.0))
         theCustomizeLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 13.0))
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = cardTableViewOutlet.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath) as! CardCell
+        cell.cardImageView.image = UIImage(named: cards[indexPath.row])
+        
+        return cell
+    }
+    
+    
     
 }
