f6df2f4b9597281953ab9867b1ee81677426e846
Darshan <>
2023-06-30 15:07:40 +0530

    new changes

--- UberPath/UberPath/Controller/Onboarding/NewCardVC.swift
+++ UberPath/UberPath/Controller/Onboarding/NewCardVC.swift
@@ -22,12 +22,10 @@ class NewCardVC: UIViewController, UITextFieldDelegate {
         vccTxtFld.delegate = self
         cardHolderTxtFld.delegate = self
         
-        registerForKeyboardNotifications()
+    
     }
     
-    deinit {
-        unregisterFromKeyboardNotifications()
-    }
+   
     
     @IBAction func countryShowBtnPressed(_ sender: UIButton) {
         // Handle country button action
@@ -57,53 +55,11 @@ class NewCardVC: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    // MARK: - Keyboard Handling
+   
+
+
+
     
-    func registerForKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    func unregisterFromKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc func keyboardWillShow(_ notification: Notification) {
-        guard let userInfo = notification.userInfo,
-              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
-            return
-        }
-        
-        // Calculate the height of the keyboard
-        let keyboardHeight = keyboardFrame.height
-        
-        // Adjust the scroll view content insets to avoid the keyboard
-        scrollView.contentInset.bottom = keyboardHeight
-        scrollView.scrollIndicatorInsets.bottom = keyboardHeight
-        
-        // Scroll the active text field into view
-        if let activeTextField = findActiveTextField() {
-            let visibleContentHeight = scrollView.bounds.height - keyboardHeight
-            
-            // Check if the active text field is hidden by the keyboard
-            if !(0 ..< visibleContentHeight).contains(activeTextField.frame.maxY) {
-                let scrollOffset = CGPoint(x: 0, y: activeTextField.frame.maxY - visibleContentHeight)
-                scrollView.setContentOffset(scrollOffset, animated: true)
-            }
-        }
-    }
-    
-    @objc func keyboardWillHide(_ notification: Notification) {
-        // Reset the scroll view content insets
-        scrollView.contentInset = .zero
-        scrollView.scrollIndicatorInsets = .zero
-    }
-    
-    func findActiveTextField() -> UITextField? {
-        // Find the active text field among the defined text fields
-        let textFields: [UITextField] = [cardNumberTxtFld, expiryDateTxtFld, vccTxtFld, cardHolderTxtFld]
-        return textFields.first { $0.isFirstResponder }
-    }
+  
 }
 
