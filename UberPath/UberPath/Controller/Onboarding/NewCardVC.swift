250c319963d3ff1d6c42c13e1956af6541313970
Darshan <>
2023-06-30 16:47:49 +0530

    new changes

--- UberPath/UberPath/Controller/Onboarding/NewCardVC.swift
+++ UberPath/UberPath/Controller/Onboarding/NewCardVC.swift
@@ -22,7 +22,12 @@ class NewCardVC: UIViewController, UITextFieldDelegate {
         vccTxtFld.delegate = self
         cardHolderTxtFld.delegate = self
         
+        registerForKeyboardNotifications()
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterFromKeyboardNotifications()
     }
     
     @IBAction func countryShowBtnPressed(_ sender: UIButton) {
@@ -53,11 +58,55 @@ class NewCardVC: UIViewController, UITextFieldDelegate {
         return true
     }
     
-   
-
-
-
+    // MARK: - Keyboard Handling
     
-  
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        let keyboardHeight = keyboardFrame.height
+        let safeAreaBottomInset = view.safeAreaInsets.bottom
+        
+        scrollView.contentInset.bottom = keyboardHeight - safeAreaBottomInset
+        scrollView.scrollIndicatorInsets.bottom = keyboardHeight - safeAreaBottomInset
+        
+        if let activeTextField = findActiveTextField(),
+           let activeTextFieldFrame = activeTextField.superview?.convert(activeTextField.frame, to: scrollView) {
+            let visibleContentHeight = max(0, scrollView.bounds.height - keyboardHeight + safeAreaBottomInset)
+            
+            if !(0...visibleContentHeight).contains(activeTextFieldFrame.maxY) {
+                let scrollOffset = CGPoint(x: 0, y: activeTextFieldFrame.maxY - visibleContentHeight)
+                scrollView.setContentOffset(scrollOffset, animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        scrollView.contentInset = .zero
+        scrollView.scrollIndicatorInsets = .zero
+    }
+    
+    func findActiveTextField() -> UITextField? {
+        let textFields: [UITextField] = [
+            cardNumberTxtFld,
+            expiryDateTxtFld,
+            vccTxtFld,
+            cardHolderTxtFld
+        ]
+        
+        return textFields.first { $0.isFirstResponder }
+    }
 }
 
