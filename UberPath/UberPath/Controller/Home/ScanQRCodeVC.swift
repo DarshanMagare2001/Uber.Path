790c5752304609463934e3026fce15273e58d1dc
Darshan <>
2023-07-04 15:34:47 +0530

    new changes

--- UberPath/UberPath/Controller/Home/ScanQRCodeVC.swift
+++ UberPath/UberPath/Controller/Home/ScanQRCodeVC.swift
@@ -8,17 +8,23 @@
 import UIKit
 
 class ScanQRCodeVC: UIViewController {
-
+    
+    @IBOutlet weak var warninglbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateFont()
         
     }
     
-
+    
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
         
     }
+    func updateFont(){
+        warninglbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 15.0))
+        
+    }
+    
     
 }
