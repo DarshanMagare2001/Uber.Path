1fe6e763238aa4f26a54b22857f897eca07b1189
Darshan <>
2023-07-04 11:00:55 +0530

    new changes

--- UberPath/UberPath/Controller/Home/MainTabVC.swift
+++ UberPath/UberPath/Controller/Home/MainTabVC.swift
@@ -20,51 +20,80 @@ class MainTabVC: UIViewController {
     @IBOutlet weak var profileBtn: UIButton!
     @IBOutlet weak var profileBtnLbl: UILabel!
     
+    enum Tab {
+        case home
+        case myCard
+        case activity
+        case profile
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showTab(.home)
         showHome()
     }
     
     @IBAction func tabBtnPressed(_ sender: UIButton) {
         let tag = sender.tag
         print(tag)
-        if tag == 1{
-            homeBtn.tintColor = UIColor(named: "LaunchscreenColor")
-            homeBtnLbl.textColor = UIColor(named: "LaunchscreenColor")
-            myCardBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            activityBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            profileBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            guard let destinationVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC") as? HomeVC else { return }
-            mainContentView.addSubview(destinationVC.view)
-        }else if tag == 2{
-            homeBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            myCardBtnLbl.textColor = UIColor(named: "LaunchscreenColor")
-            activityBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            profileBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            guard let destinationVC = self.storyboard?.instantiateViewController(withIdentifier: "MyCardVC") as? MyCardVC else { return }
-            mainContentView.addSubview(destinationVC.view)
-        }else if tag == 3{
-            homeBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            myCardBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            activityBtnLbl.textColor = UIColor(named: "LaunchscreenColor")
-            profileBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            guard let destinationVC = self.storyboard?.instantiateViewController(withIdentifier: "ActivityVC") as? ActivityVC else { return }
-            mainContentView.addSubview(destinationVC.view)
-        }else if tag == 4{
-            homeBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            myCardBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            activityBtnLbl.textColor = UIColor(named: "GlobalButtonColor")
-            profileBtnLbl.textColor = UIColor(named: "LaunchscreenColor")
-            guard let destinationVC = self.storyboard?.instantiateViewController(withIdentifier: "ProfileVC") as? ProfileVC else { return }
-            mainContentView.addSubview(destinationVC.view)
+        var selectedTab: Tab?
+        
+        switch tag {
+        case 1:
+            selectedTab = .home
+        case 2:
+            selectedTab = .myCard
+        case 3:
+            selectedTab = .activity
+        case 4:
+            selectedTab = .profile
+        default:
+            break
         }
         
+        guard let tab = selectedTab else {
+            return
+        }
+        
+        updateUI(for: tab)
+        showTab(tab)
     }
     
-    func showHome(){
-        guard let destinationVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC") as? HomeVC else { return }
-        mainContentView.addSubview(destinationVC.view)
+    func updateUI(for tab: Tab) {
+        homeBtnLbl.textColor = tab == .home ? UIColor(named: "LaunchscreenColor") : UIColor(named: "GlobalButtonColor")
+        homeBtn.tintColor = tab == .home ? UIColor(named: "LaunchscreenColor") : UIColor.lightGray
+        myCardBtnLbl.textColor = tab == .myCard ? UIColor(named: "LaunchscreenColor") : UIColor(named: "GlobalButtonColor")
+        myCardBtn.tintColor = tab == .myCard ? UIColor(named: "LaunchscreenColor") : UIColor.lightGray
+        activityBtnLbl.textColor = tab == .activity ? UIColor(named: "LaunchscreenColor") : UIColor(named: "GlobalButtonColor")
+        activityBtn.tintColor = tab == .activity ? UIColor(named: "LaunchscreenColor") : UIColor.lightGray
+        profileBtnLbl.textColor = tab == .profile ? UIColor(named: "LaunchscreenColor") : UIColor(named: "GlobalButtonColor")
+        profileBtn.tintColor = tab == .profile ? UIColor(named: "LaunchscreenColor") : UIColor.lightGray
     }
     
+    
+    func showTab(_ tab: Tab) {
+        var destinationVC: UIViewController?
+        
+        switch tab {
+        case .home:
+            destinationVC = storyboard?.instantiateViewController(withIdentifier: "HomeVC") as? HomeVC
+        case .myCard:
+            destinationVC = storyboard?.instantiateViewController(withIdentifier: "MyCardVC") as? MyCardVC
+        case .activity:
+            destinationVC = storyboard?.instantiateViewController(withIdentifier: "ActivityVC") as? ActivityVC
+        case .profile:
+            destinationVC = storyboard?.instantiateViewController(withIdentifier: "ProfileVC") as? ProfileVC
+        }
+        
+        if let destinationVC = destinationVC {
+            mainContentView.addSubview(destinationVC.view)
+        }
+    }
+    
+    func showHome() {
+        showTab(.home)
+        homeBtnLbl.textColor = UIColor(named: "LaunchscreenColor")
+        homeBtn.tintColor  = UIColor(named: "LaunchscreenColor")
+    }
 }
+
