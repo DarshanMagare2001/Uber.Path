b2f85bfb9f8f29f21332565eb12d8746b06bc443
Darshan <>
2023-07-05 17:44:36 +0530

    new changes

--- UberPath/UberPath/Controller/Home/TransfersVC/ChooseRecipientsCell.swift
+++ UberPath/UberPath/Controller/Home/TransfersVC/ChooseRecipientsCell.swift
@@ -9,5 +9,24 @@ import UIKit
 
 class ChooseRecipientsCell: UICollectionViewCell {
     @IBOutlet weak var cellImage: UIImageView!
-    @IBOutlet weak var cellLbl: UILabel!
+       @IBOutlet weak var cellLbl: UILabel!
+       @IBOutlet weak var cellView: RoundedButtonWithBorder!
+       
+       override func awakeFromNib() {
+           super.awakeFromNib()
+           
+           let tapGesture = UITapGestureRecognizer(target: self, action: #selector(cellViewTapped))
+           cellView.addGestureRecognizer(tapGesture)
+       }
+       
+    @objc func cellViewTapped() {
+        if cellView.backgroundColor == .green {
+            cellView.backgroundColor = .white
+        } else {
+            cellView.backgroundColor = .green
+        }
+        // Handle tap on cellView here...
+    }
+
+
 }
