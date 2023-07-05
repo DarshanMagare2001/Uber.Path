902799bab4f4ab804fadfe307acc137300a439dc
Darshan <>
2023-07-05 12:04:40 +0530

    new changes

---
+++ UberPath/UberPath/Controller/Home/TransfersVC/ChooseCardsCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChooseCardsCell.swift
+//  UberPath
+//
+//  Created by IPS-161 on 05/07/23.
+//
+
+import UIKit
+
+class ChooseCardsCell: UICollectionViewCell {
+    
+}
---
+++ UberPath/UberPath/Controller/Home/TransfersVC/ChooseRecipientsCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChooseRecipientsCell.swift
+//  UberPath
+//
+//  Created by IPS-161 on 05/07/23.
+//
+
+import UIKit
+
+class ChooseRecipientsCell: UICollectionViewCell {
+    
+}
