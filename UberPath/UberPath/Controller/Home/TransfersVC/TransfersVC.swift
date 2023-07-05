390d2e908dbfb007dede82e06502685cef15d79c
Darshan <>
2023-07-05 14:44:37 +0530

    new changes

--- UberPath/UberPath/Controller/Home/TransfersVC/TransfersVC.swift
+++ UberPath/UberPath/Controller/Home/TransfersVC/TransfersVC.swift
@@ -1,20 +1,15 @@
-//
-//  TransfersVC.swift
-//  UberPath
-//
-//  Created by IPS-161 on 04/07/23.
-//
-
 import UIKit
 
-class TransfersVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource , UITextFieldDelegate {
+class TransfersVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UITextFieldDelegate {
     @IBOutlet weak var collectionViewOutlet: UICollectionView!
     @IBOutlet weak var recipientsCollectionView: UICollectionView!
     @IBOutlet weak var chooseCardsLbl: UILabel!
     @IBOutlet weak var chooseRecepientLbl: UILabel!
     @IBOutlet weak var textFld: UITextField!
-    var collectionViewOutletArray = ["CardStyleOne","CardStyleThree","CardStyleTwo"]
-    var recipientsCollectionViewArray = ["Linda John","David William","Susan Charles"]
+    @IBOutlet weak var scrollView: UIScrollView!
+    var collectionViewOutletArray = ["CardStyleOne", "CardStyleThree", "CardStyleTwo"]
+    var recipientsCollectionViewArray = ["Linda John", "David William", "Susan Charles"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateFont()
@@ -23,7 +18,13 @@ class TransfersVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
         recipientsCollectionView.delegate = self
         recipientsCollectionView.dataSource = self
         textFld.delegate = self
-        updatePlaceHolders()
+        updatePlaceholders()
+        registerForKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterFromKeyboardNotifications()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -37,7 +38,7 @@ class TransfersVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == collectionViewOutlet {
-            let cell = collectionViewOutlet.dequeueReusableCell(withReuseIdentifier: "cardCollectionCell", for: indexPath) as!ChooseCardsCell
+            let cell = collectionViewOutlet.dequeueReusableCell(withReuseIdentifier: "cardCollectionCell", for: indexPath) as! ChooseCardsCell
             cell.cellImage.image = UIImage(named: collectionViewOutletArray[indexPath.row])
             return cell
         } else if collectionView == recipientsCollectionView {
@@ -46,23 +47,66 @@ class TransfersVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
             cell.cellLbl.text = recipientsCollectionViewArray[indexPath.row]
             return cell
         }
-        
         return UICollectionViewCell()
     }
     
     func updateFont() {
         chooseCardsLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 20.0))
         chooseRecepientLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 20.0))
-        
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    func updatePlaceHolders(){
+    
+    func updatePlaceholders() {
         textFld.placeholder = "Search contacts..."
     }
     
+    // MARK: - Keyboard Handling
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        let keyboardHeight = keyboardFrame.height
+        let safeAreaBottomInset = view.safeAreaInsets.bottom
+        
+        scrollView.contentInset.bottom = keyboardHeight - safeAreaBottomInset
+        scrollView.scrollIndicatorInsets.bottom = keyboardHeight - safeAreaBottomInset
+        
+        if let activeTextField = findActiveTextField(),
+           let activeTextFieldFrame = activeTextField.superview?.convert(activeTextField.frame, to: scrollView) {
+            let visibleContentHeight = max(0, scrollView.bounds.height - keyboardHeight + safeAreaBottomInset)
+            
+            if !(0...visibleContentHeight).contains(activeTextFieldFrame.maxY) {
+                let scrollOffset = CGPoint(x: 0, y: activeTextFieldFrame.maxY - visibleContentHeight)
+                scrollView.setContentOffset(scrollOffset, animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        scrollView.contentInset = .zero
+        scrollView.scrollIndicatorInsets = .zero
+    }
+    
+    func findActiveTextField() -> UITextField? {
+        let textFields: [UITextField] = [textFld]
+        return textFields.first { $0.isFirstResponder }
+    }
 }
 
