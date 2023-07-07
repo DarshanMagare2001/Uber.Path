54fa22d9a4d1b7fa916623c8a1efcd65f81f185d
Darshan <>
2023-07-07 11:32:04 +0530

    new changes

--- UberPath/UberPath/App/AppDelegate.swift
+++ UberPath/UberPath/App/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
+        IQKeyboardManager.shared.enable = true
         return true
     }
     
@@ -36,9 +38,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         return GIDSignIn.sharedInstance().handle(url)
     }
-    
-    
-    
-    
+     
 }
 
