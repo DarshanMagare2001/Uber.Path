050cb0635a9fc04fe1e0a732e0284deb6f1ff30f
Darshan <>
2023-07-10 14:36:21 +0530

    new

--- UberPath/UberPath/Controller/Home/MyCard/EditCardVC.swift
+++ UberPath/UberPath/Controller/Home/MyCard/EditCardVC.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class EditCardVC: UIViewController {
     var cardImage : String?
+    @IBOutlet weak var personLblView: UIView!
+    @IBOutlet weak var personLbl: UILabel!
+    @IBOutlet weak var manageLblView: UIView!
+    @IBOutlet weak var manageLbl: UILabel!
+    @IBOutlet weak var detailLblView: UIView!
+    @IBOutlet weak var detailLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateFont()
         
     }
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-
+    
+    func updateFont(){
+        personLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 10.0))
+        manageLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 10.0))
+        detailLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 10.0))
+    }
+    
 }
