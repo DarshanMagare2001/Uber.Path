2be6d2ff71264a977b49a70723a8a08b5c453ee5
Darshan <>
2023-07-11 17:07:09 +0530

    new

--- UberPath/UberPath/Controller/Home/Profile/ReferralCode/ReferralCodeVC.swift
+++ UberPath/UberPath/Controller/Home/Profile/ReferralCode/ReferralCodeVC.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class ReferralCodeVC: UIViewController {
-
+    @IBOutlet weak var heresLb: UILabel!
+    @IBOutlet weak var shareLbl: UILabel!
+    @IBOutlet weak var getLbl: UILabel!
+    @IBOutlet weak var forAmountLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateFont()
         
     }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-   
-
+    
+    func updateFont() {
+        heresLb.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        shareLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 13.0))
+        getLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        forAmountLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 13.0))
+    }
+    
 }
