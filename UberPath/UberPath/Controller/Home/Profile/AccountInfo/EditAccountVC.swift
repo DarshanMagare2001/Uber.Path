27e6dd2f49251bad10cb24598668fc3519bcbfe1
Darshan <>
2023-07-11 11:22:38 +0530

    new

--- UberPath/UberPath/Controller/Home/Profile/AccountInfo/EditAccountVC.swift
+++ UberPath/UberPath/Controller/Home/Profile/AccountInfo/EditAccountVC.swift
@@ -8,14 +8,38 @@
 import UIKit
 
 class EditAccountVC: UIViewController {
-
+    @IBOutlet weak var yourNameLbl: UILabel!
+    @IBOutlet weak var OccupationLbl: UILabel!
+    @IBOutlet weak var employerLbl: UILabel!
+    @IBOutlet weak var phoneNumberLbl: UILabel!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var yourNameTxtFld: UITextField!
+    @IBOutlet weak var OccupationTxtFld: UITextField!
+    @IBOutlet weak var employerTxtFld: UITextField!
+    @IBOutlet weak var phoneNumberTxtFld: UITextField!
+    @IBOutlet weak var emailTxtFld: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateFont()
         
     }
     
-
     
-
+    
+    @IBAction func saveBtnPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func updateFont() {
+        yourNameLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        OccupationLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        employerLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        phoneNumberLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+        emailLbl.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 17.0))
+    }
+    
 }
