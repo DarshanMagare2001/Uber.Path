037faec756fbba203ec6c88c7b6844dbcaa3c186
Darshan <>
2023-07-11 11:09:20 +0530

    new

--- UberPath/UberPath/Controller/Home/Profile/AccountInfo/AccountInfoVC.swift
+++ UberPath/UberPath/Controller/Home/Profile/AccountInfo/AccountInfoVC.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class AccountInfoVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
- 
+    
+    
+    
+    @IBAction func editBtnPressed(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "EditAccountVC") as! EditAccountVC
+        navigationController?.pushViewController(destinationVC, animated: true)
+    }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
---
+++ UberPath/UberPath/Controller/Home/Profile/AccountInfo/EditAccountVC.swift
@@ -0,0 +1,21 @@
+//
+//  EditAccountVC.swift
+//  UberPath
+//
+//  Created by IPS-161 on 11/07/23.
+//
+
+import UIKit
+
+class EditAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
