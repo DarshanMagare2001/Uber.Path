0b4037571a4394caddddda2770d1eac96df3c477
Darshan <>
2023-07-12 17:51:13 +0530

    new

--- UberPath/UberPath/Controller/Home/Profile/Language/LanguageVC.swift
+++ UberPath/UberPath/Controller/Home/Profile/Language/LanguageVC.swift
@@ -15,9 +15,9 @@ class LanguageVC: UIViewController {
     let countryKit = CountryKit()
     var countriesArray = [Country]()
     var filteredCountriesArray = [Country]()
-    var tappedButtonIndex: Int?
-    
-    
+    var selectedCountry: Country?
+    var searchText: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
         countriesArray = countryKit.countries
@@ -26,61 +26,73 @@ class LanguageVC: UIViewController {
         searchTxtFld.addTarget(self, action: #selector(searchTextDidChange), for: .editingChanged)
         searchTxtFld.placeholder = "Search"
     }
-    
+
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-    
+
     @IBAction func searchBtnPressed(_ sender: UIButton) {
         searchTxtFld.resignFirstResponder()
     }
-    
+
     @objc func searchTextDidChange() {
-        if let searchText = searchTxtFld.text, !searchText.isEmpty {
-            filteredCountriesArray = countriesArray.filter { $0.name.lowercased().contains(searchText.lowercased()) }
-        } else {
+        searchText = searchTxtFld.text?.lowercased() ?? ""
+
+        if searchText.isEmpty {
             filteredCountriesArray = countriesArray
+        } else {
+            filteredCountriesArray = countriesArray.filter { $0.name.lowercased().contains(searchText) }
         }
-        
+
         tableViewOutlet.reloadData()
     }
-    
 }
 
 extension LanguageVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredCountriesArray.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "languageVCCell", for: indexPath) as! LanguageVCCell
         let country = filteredCountriesArray[indexPath.row]
-        
+
         if let flagImage = country.flagImage {
             cell.countryImage.image = flagImage
         } else {
             cell.countryImage.image = UIImage(named: "defaultFlag")
         }
-        
+
         cell.countryName.text = country.name
-        
+
         let language = CountryModel().countryLanguages[country.name] ?? ""
         cell.countryLanguageLbl.text = language
-        
+
         // Update button appearance based on selection
-        if let tappedButtonIndex = tappedButtonIndex, indexPath.row == tappedButtonIndex {
-            cell.cellBtn.isSelected = true
-        } else {
-            cell.cellBtn.isSelected = false
-        }
-        
+        cell.cellBtn.isSelected = (selectedCountry == country)
+
         // Set the buttonTapAction closure
         cell.buttonTapAction = { [weak self] in
-            self?.tappedButtonIndex = indexPath.row
-            tableView.reloadData()
+            if let weakSelf = self {
+                if weakSelf.selectedCountry == country {
+                    weakSelf.selectedCountry = nil
+                } else {
+                    weakSelf.selectedCountry = country
+                }
+
+                if weakSelf.searchTxtFld.isFirstResponder {
+                    weakSelf.searchTxtFld.resignFirstResponder()
+                }
+                tableView.reloadData()
+            }
         }
-        
+
         return cell
     }
 }
 
+extension Country: Equatable {
+    public static func == (lhs: Country, rhs: Country) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
