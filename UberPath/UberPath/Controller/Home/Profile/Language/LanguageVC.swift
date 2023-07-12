1230784e8f21ec9f4ff4ff057b8cdcf46ec3a824
Darshan <>
2023-07-12 10:05:42 +0530

    ne

--- UberPath/UberPath/Controller/Home/Profile/Language/LanguageVC.swift
+++ UberPath/UberPath/Controller/Home/Profile/Language/LanguageVC.swift
@@ -6,17 +6,31 @@
 //
 
 import UIKit
+import ADCountryPicker
 
 class LanguageVC: UIViewController {
-
+    @IBOutlet weak var tableViewOutlet: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-
+    
+}
+extension LanguageVC : UITableViewDelegate , UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "languageVCCell", for: indexPath) as! LanguageVCCell
+        return cell
+        
+    }
+    
+    
 }
---
+++ UberPath/UberPath/Controller/Home/Profile/Language/LanguageVCCell.swift
@@ -0,0 +1,25 @@
+//
+//  LanguageVCCell.swift
+//  UberPath
+//
+//  Created by IPS-161 on 12/07/23.
+//
+
+import UIKit
+
+class LanguageVCCell: UITableViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellLbl: UILabel!
+    @IBOutlet weak var cellBtn: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+    }
+    
+}
