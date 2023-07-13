0181c0ba7a1e0d162850af66ac75eef81539e537
Darshan <>
2023-07-13 18:05:03 +0530

    new

--- UberPath/UberPath/Controller/Home/Profile/Add FingerPrint/AddFingerPrintVC.swift
+++ UberPath/UberPath/Controller/Home/Profile/Add FingerPrint/AddFingerPrintVC.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class AddFingerPrintVC: UIViewController {
-    
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateFont()
         
     }
     
@@ -23,5 +24,8 @@ class AddFingerPrintVC: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
-    
+    func updateFont(){
+        lbl1.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 18.0))
+        lbl2.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 15.0))
+    }
 }
