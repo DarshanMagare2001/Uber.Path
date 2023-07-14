f5128212887d4af6593a1588b667df0f5d7f6569
Darshan <>
2023-07-14 12:38:20 +0530

    new

--- UberPath/UberPath/Controller/Home/Activity/ActivityVC.swift
+++ UberPath/UberPath/Controller/Home/Activity/ActivityVC.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 class ActivityVC: UIViewController {
-
+    @IBOutlet weak var collectionViewOne: UICollectionView!
+    @IBOutlet weak var pageControllForCollectionViewOne: UIPageControl!
+    var collectionViewOneArray = ["Co.payment Cards","Smartpay Cards"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-  
     }
     
+}
 
+extension ActivityVC : UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionViewOneArray.count
+    }
     
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewOneCell", for: indexPath) as! CollectionViewOneCell
+        cell.cellImg.image = UIImage(named : collectionViewOneArray[indexPath.row])
+        return cell
+    }
 }
---
+++ UberPath/UberPath/Controller/Home/Activity/CollectionViewOneCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionView1Cell.swift
+//  UberPath
+//
+//  Created by IPS-161 on 14/07/23.
+//
+
+import UIKit
+
+class CollectionViewOneCell: UICollectionViewCell {
+    @IBOutlet weak var cellImg: UIImageView!
+}
