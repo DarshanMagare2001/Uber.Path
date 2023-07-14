105f1535a6f3c4e7fd26137882e961c4e4089a5b
Darshan <>
2023-07-14 12:55:08 +0530

    new

--- UberPath/UberPath/Controller/Home/Activity/ActivityVC.swift
+++ UberPath/UberPath/Controller/Home/Activity/ActivityVC.swift
@@ -10,35 +10,51 @@ import UIKit
 class ActivityVC: UIViewController {
     @IBOutlet weak var collectionViewOne: UICollectionView!
     @IBOutlet weak var pageControllForCollectionViewOne: UIPageControl!
-    var collectionViewOneArray = ["Co.payment Cards","Smartpay Cards"]
+    var collectionViewOneArray = ["Co.payment Cards", "Smartpay Cards"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionViewOne.delegate = self
         collectionViewOne.dataSource = self
         pageControllForCollectionViewOne.numberOfPages = collectionViewOneArray.count
+        
+        let layout = UICollectionViewFlowLayout()
+        let cellWidth = collectionViewOne.frame.width - 40 // Adjust the width as per your requirement
+        layout.itemSize = CGSize(width: cellWidth, height: collectionViewOne.frame.height)
+        layout.minimumLineSpacing = 20 // Adjust the spacing as per your requirement
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20) // Adjust the insets as per your requirement
+        layout.scrollDirection = .horizontal
+        collectionViewOne.collectionViewLayout = layout
     }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageIndex = round(scrollView.contentOffset.x / scrollView.frame.width)
-        pageControllForCollectionViewOne.currentPage = Int(pageIndex)
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageWidth = scrollView.frame.width
+        let currentPage = Int(scrollView.contentOffset.x / pageWidth)
+        pageControllForCollectionViewOne.currentPage = currentPage
     }
     
     @IBAction func pageControlValueChanged(_ sender: UIPageControl) {
-           let page = sender.currentPage
-           let indexPath = IndexPath(item: page, section: 0)
-           collectionViewOne.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-       }
-    
+        let page = sender.currentPage
+        let offsetX = CGFloat(page) * collectionViewOne.frame.width
+        let offset = CGPoint(x: offsetX, y: 0)
+        collectionViewOne.setContentOffset(offset, animated: true)
+    }
 }
 
-extension ActivityVC : UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
+extension ActivityVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return collectionViewOneArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewOneCell", for: indexPath) as! CollectionViewOneCell
-        cell.cellImg.image = UIImage(named : collectionViewOneArray[indexPath.row])
+        cell.cellImg.image = UIImage(named: collectionViewOneArray[indexPath.row])
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = collectionView.frame.width - 40 // Adjust the width as per your requirement
+        return CGSize(width: cellWidth, height: collectionView.frame.height)
+    }
+    
 }
