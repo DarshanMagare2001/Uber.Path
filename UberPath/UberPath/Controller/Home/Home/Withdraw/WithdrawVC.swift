3af8a231e1d4592cead14a77c02e9a14d2c7e9fd
Darshan <>
2023-07-17 17:28:51 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/Withdraw/WithdrawVC.swift
+++ UberPath/UberPath/Controller/Home/Home/Withdraw/WithdrawVC.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 class WithdrawVC: UIViewController {
-
+    @IBOutlet weak var btn1: RoundedButtonWithBorder!
+    @IBOutlet weak var btn2: RoundedButtonWithBorder!
+    @IBOutlet weak var btn3: RoundedButtonWithBorder!
+    @IBOutlet weak var btn4: RoundedButtonWithBorder!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-    
-
 }
