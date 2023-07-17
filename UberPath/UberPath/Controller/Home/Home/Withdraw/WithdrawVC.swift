2faa274376c6aaa2b8b9881c66f50b9253c67af2
Darshan <>
2023-07-17 17:38:04 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/Withdraw/WithdrawVC.swift
+++ UberPath/UberPath/Controller/Home/Home/Withdraw/WithdrawVC.swift
@@ -14,11 +14,41 @@ class WithdrawVC: UIViewController {
     @IBOutlet weak var btn4: RoundedButtonWithBorder!
     override func viewDidLoad() {
         super.viewDidLoad()
+        btn1.applyRoundedCornerAndShadow()
+        btn2.applyRoundedCornerAndShadow()
+        btn3.applyRoundedCornerAndShadow()
+        btn4.applyRoundedCornerAndShadow()
         
-        
+    }
+    
+    @IBAction func percentageBtnPressed(_ sender: UIButton) {
+        // Reset all buttons to their default state
+        let color = UIColor(named: "LaunchscreenColor")
+        btn1.backgroundColor = .systemGray6
+        btn1.tintColor = .black
+        btn2.backgroundColor = .systemGray6
+        btn2.tintColor = .black
+        btn3.backgroundColor = .systemGray6
+        btn3.tintColor = .black
+        btn4.backgroundColor = .systemGray6
+        btn4.tintColor = .black
+        // Set the tapped button's background color to green
+        sender.backgroundColor = color
+        sender.tintColor = .white
     }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension UIButton {
+    func applyRoundedCornerAndShadow() {
+        layer.cornerRadius = frame.height / 2
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 2
     }
 }
