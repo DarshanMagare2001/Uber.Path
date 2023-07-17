6e3c1772bceffbfdeac5aa65ebcb41a1624cf964
Darshan <>
2023-07-17 17:40:50 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/Withdraw/WithdrawVC.swift
+++ UberPath/UberPath/Controller/Home/Home/Withdraw/WithdrawVC.swift
@@ -12,43 +12,45 @@ class WithdrawVC: UIViewController {
     @IBOutlet weak var btn2: RoundedButtonWithBorder!
     @IBOutlet weak var btn3: RoundedButtonWithBorder!
     @IBOutlet weak var btn4: RoundedButtonWithBorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        btn1.applyRoundedCornerAndShadow()
-        btn2.applyRoundedCornerAndShadow()
-        btn3.applyRoundedCornerAndShadow()
-        btn4.applyRoundedCornerAndShadow()
-        
+        configureBtns()
     }
     
     @IBAction func percentageBtnPressed(_ sender: UIButton) {
-        // Reset all buttons to their default state
-        let color = UIColor(named: "LaunchscreenColor")
-        btn1.backgroundColor = .systemGray6
-        btn1.tintColor = .black
-        btn2.backgroundColor = .systemGray6
-        btn2.tintColor = .black
-        btn3.backgroundColor = .systemGray6
-        btn3.tintColor = .black
-        btn4.backgroundColor = .systemGray6
-        btn4.tintColor = .black
-        // Set the tapped button's background color to green
-        sender.backgroundColor = color
+        resetButtonsToDefaultState()
+        sender.backgroundColor = UIColor(named: "LaunchscreenColor")
         sender.tintColor = .white
     }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-}
-
-extension UIButton {
-    func applyRoundedCornerAndShadow() {
-        layer.cornerRadius = frame.height / 2
-        layer.masksToBounds = false
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOpacity = 0.5
-        layer.shadowOffset = CGSize(width: 0, height: 2)
-        layer.shadowRadius = 2
+    
+    func configureBtns(){
+        applyRoundedCornerAndShadow(to: btn1)
+        applyRoundedCornerAndShadow(to: btn2)
+        applyRoundedCornerAndShadow(to: btn3)
+        applyRoundedCornerAndShadow(to: btn4)
+    }
+    
+    func applyRoundedCornerAndShadow(to button: UIButton) {
+        button.layer.cornerRadius = button.frame.height / 2
+        button.layer.masksToBounds = false
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.5
+        button.layer.shadowOffset = CGSize(width: 0, height: 2)
+        button.layer.shadowRadius = 2
+    }
+    
+    func resetButtonsToDefaultState() {
+        let defaultColor = UIColor.systemGray6
+        let defaultTintColor = UIColor.black
+        
+        [btn1, btn2, btn3, btn4].forEach { button in
+            button?.backgroundColor = defaultColor
+            button?.tintColor = defaultTintColor
+        }
     }
 }
