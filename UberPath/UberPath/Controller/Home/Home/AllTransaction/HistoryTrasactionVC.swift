c119702e88ad647b5549769e8f7b23c4b06c34fd
Darshan <>
2023-07-18 12:49:28 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/AllTransaction/HistoryTrasactionVC.swift
+++ UberPath/UberPath/Controller/Home/Home/AllTransaction/HistoryTrasactionVC.swift
@@ -8,16 +8,30 @@
 import UIKit
 
 class HistoryTrasactionVC: UIViewController {
-
+    @IBOutlet weak var amountShowLblView: UIView!
+    @IBOutlet weak var amountHideShowBtn: UIButton!
+    var isAmountShow = true
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        amountShowLblView.isHidden = false
         
     }
     
-
+    @IBAction func amountHideShowBtnPressed(_ sender: UIButton) {
+        amountShowLblView.isHidden.toggle()
+        isAmountShow.toggle()
+        if isAmountShow {
+            let image = UIImage(systemName: "eye")
+            amountHideShowBtn.setImage(image, for: .normal)
+        }else{
+            let image = UIImage(systemName: "eye.slash")
+            amountHideShowBtn.setImage(image, for: .normal)
+        }
+    }
+    
+    
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-
+    
 }
