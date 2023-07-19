1807e5f49d8aaa954548841c0785b00abf5dfe87
Darshan <>
2023-07-19 12:45:04 +0530

    new

---
+++ UberPath/UberPath/Controller/Home/Home/More/ATM/ATMFinderDetailVC.swift
@@ -0,0 +1,23 @@
+//
+//  ATMFinderDetailVC.swift
+//  UberPath
+//
+//  Created by IPS-161 on 19/07/23.
+//
+
+import UIKit
+
+class ATMFinderDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+  
+
+}
--- UberPath/UberPath/Controller/Home/Home/More/ATM/ATMFinderVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/ATM/ATMFinderVC.swift
@@ -20,6 +20,13 @@ class ATMFinderVC: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    
+    @IBAction func nowBtnPressed(_ sender: UIButton) {
+        let storyboard: UIStoryboard = UIStoryboard(name: "Home", bundle: nil)
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "ATMFinderDetailVC") as! ATMFinderDetailVC
+        navigationController?.pushViewController(destinationVC, animated: true)
+    }
+    
     func updateFont(){
         lbl1.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 25.0))
         lbl2.font = UIFont.systemFont(ofSize: FontManager.adjustedFontSize(forBaseSize: 14.0))
