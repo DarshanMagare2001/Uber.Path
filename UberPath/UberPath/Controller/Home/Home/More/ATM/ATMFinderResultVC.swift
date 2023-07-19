143e13824b5d46b194c4e0aeaf249fb77881343e
Darshan <>
2023-07-19 16:05:22 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/More/ATM/ATMFinderResultVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/ATM/ATMFinderResultVC.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ATMFinderResultVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
-   
-
+    @IBAction func getDirectionBtnPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
