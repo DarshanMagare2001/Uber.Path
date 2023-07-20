2c6dcd50b876af9c5d7a3fbbc673a01899a9616a
Darshan <>
2023-07-20 14:09:01 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/CardTopUpVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/CardTopUpVC.swift
@@ -15,7 +15,7 @@ class CardTopUpVC: UIViewController {
     @IBOutlet weak var lbl1: RoundedLabelWithBorder!
     @IBOutlet weak var lbl2: RoundedLabelWithBorder!
     @IBOutlet weak var lbl3: RoundedLabelWithBorder!
-    
+    var tappedLabel: RoundedLabelWithBorder? // Keep track of the tapped label
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,16 +44,45 @@ class CardTopUpVC: UIViewController {
     }
     
     @objc func labelTapped(_ sender: UITapGestureRecognizer) {
-        // Change the background color to green
-        if let tappedLabel = sender.view as? UILabel {
-            tappedLabel.backgroundColor = UIColor.green
-            
-            // Add shadow effect
+        // Reset all labels
+        resetAllLabels()
+        
+        // Change the background color to green and add shadow effect for the tapped label
+        if let tappedLabel = sender.view as? RoundedLabelWithBorder {
+            tappedLabel.backgroundColor = UIColor(named: "LaunchscreenColor")
+            tappedLabel.textColor = .white
             tappedLabel.layer.shadowColor = UIColor.black.cgColor
             tappedLabel.layer.shadowOpacity = 0.5
             tappedLabel.layer.shadowOffset = CGSize(width: 0, height: 2)
             tappedLabel.layer.shadowRadius = 4
             tappedLabel.layer.shadowPath = UIBezierPath(roundedRect: tappedLabel.bounds, cornerRadius: tappedLabel.layer.cornerRadius).cgPath
+            
+            // Update the currently tapped label
+            self.tappedLabel = tappedLabel
         }
+    }
+    
+    func resetAllLabels() {
+        // Reset the background color and shadow effect for all labels
+        lbl1.backgroundColor = .systemGray6
+        lbl1.textColor = UIColor(named: "GlobalButtonColor")
+        lbl1.layer.shadowColor = nil
+        lbl1.layer.shadowOpacity = 0
+        lbl1.layer.shadowOffset = CGSize.zero
+        lbl1.layer.shadowRadius = 0
+        
+        lbl2.backgroundColor = .systemGray6
+        lbl2.textColor = UIColor(named: "GlobalButtonColor")
+        lbl2.layer.shadowColor = nil
+        lbl2.layer.shadowOpacity = 0
+        lbl2.layer.shadowOffset = CGSize.zero
+        lbl2.layer.shadowRadius = 0
+        
+        lbl3.backgroundColor = .systemGray6
+        lbl3.textColor = UIColor(named: "GlobalButtonColor")
+        lbl3.layer.shadowColor = nil
+        lbl3.layer.shadowOpacity = 0
+        lbl3.layer.shadowOffset = CGSize.zero
+        lbl3.layer.shadowRadius = 0
     }
 }
