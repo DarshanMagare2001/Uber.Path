74cd6d24ec353f378944a5cc27d9daa4f72c042f
Darshan <>
2023-07-20 15:39:23 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/ConfirmTopUpVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/ConfirmTopUpVC.swift
@@ -8,15 +8,23 @@
 import UIKit
 
 class ConfirmTopUpVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
-   
+    
+    
+    @IBAction func confirmTopUpBtnPressed(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "TopupSuccessVC") as! TopupSuccessVC
+        destinationVC.modalPresentationStyle = .overFullScreen
+        present(destinationVC, animated: true, completion: nil)
+    }
+    
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-
+    
 }
---
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/TopupSuccessVC.swift
@@ -0,0 +1,20 @@
+//
+//  TopupSuccessVC.swift
+//  UberPath
+//
+//  Created by IPS-161 on 20/07/23.
+//
+
+import UIKit
+
+class TopupSuccessVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
