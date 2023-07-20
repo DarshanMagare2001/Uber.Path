54edacc9c013287795b687d66661b5656515d1fd
Darshan <>
2023-07-20 12:51:57 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/CardTopUpVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/CardTopUpVC.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class CardTopUpVC: UIViewController {
-
+    @IBOutlet weak var currencySymbolBtn: UIButton!
+    @IBOutlet weak var currencyShowLbl: UILabel!
+    
+    @IBOutlet weak var amountTxtFld: UITextField!
+    
+    @IBOutlet weak var currencySymbolPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-
+    
+    @IBAction func currencySymbolShowBtnPressed(_ sender: UIButton) {
+        
+    }
+    
 }
