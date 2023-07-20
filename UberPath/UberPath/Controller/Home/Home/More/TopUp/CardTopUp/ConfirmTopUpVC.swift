694844a7c3b5621148bc17ca55835748ef50ce9c
Darshan <>
2023-07-20 15:53:57 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/ConfirmTopUpVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/ConfirmTopUpVC.swift
@@ -7,24 +7,28 @@
 
 import UIKit
 
-class ConfirmTopUpVC: UIViewController {
-    
+class ConfirmTopUpVC: UIViewController, popToRootVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
-    
-    
     @IBAction func confirmTopUpBtnPressed(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Home", bundle: nil)
         let destinationVC = storyboard.instantiateViewController(withIdentifier: "TopupSuccessVC") as! TopupSuccessVC
         destinationVC.modalPresentationStyle = .overFullScreen
+        destinationVC.delegate = self
         present(destinationVC, animated: true, completion: nil)
     }
     
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    func popToRootVC(pop: Bool) {
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
     }
     
 }
--- UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/TopupSuccessVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/CardTopUp/TopupSuccessVC.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
-
+protocol popToRootVC {
+    func popToRootVC(pop : Bool)
+}
 class TopupSuccessVC: UIViewController {
-
+    var delegate : popToRootVC?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
-
-
+    
+    @IBAction func backToHomeBtnPressed(_ sender: UIButton) {
+       dismiss(animated: true)
+        delegate?.popToRootVC(pop: true)
+    }
 }
