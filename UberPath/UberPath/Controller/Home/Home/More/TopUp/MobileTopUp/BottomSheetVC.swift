074b225b9d715c249b31561eeb8667ca32434f7f
Darshan <>
2023-07-21 16:00:56 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/More/TopUp/MobileTopUp/BottomSheetVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/MobileTopUp/BottomSheetVC.swift
@@ -13,14 +13,7 @@ class BottomSheetVC: UIViewController {
     @IBOutlet weak var lbl2: UILabel!
     @IBOutlet weak var amountTxtFld: UITextField!
     @IBOutlet weak var slide: UISlider!
-  
-    
-    
-    
-    
-    
-    
-    
+   
     var model : MobileTopUpTableViewModel?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,8 +27,26 @@ class BottomSheetVC: UIViewController {
     @IBAction func plusBtnPressed(_ sender: UIButton) {
     }
     
-    
-    
+    @IBAction func moneyBtnPressed(_ sender: UIButton) {
+        if sender.tag == 1 {
+            amountTxtFld.text = String(5)
+        } else if sender.tag == 2 {
+            amountTxtFld.text = String(10)
+        } else if sender.tag == 3 {
+            amountTxtFld.text = String(15)
+        } else if sender.tag == 4 {
+            amountTxtFld.text = String(20)
+        } else if sender.tag == 5 {
+            amountTxtFld.text = String(50)
+        } else if sender.tag == 6 {
+            amountTxtFld.text = String(100)
+        } else if sender.tag == 7 {
+            amountTxtFld.text = String(200)
+        } else if sender.tag == 8 {
+            amountTxtFld.text = String(500)
+        }
+    }
+  
     func update(){
         guard let data = model else {return}
         img.image = UIImage(named: data.img)
