00e9a7066ebf29dd8892c7904964f020de03fe6a
Darshan <>
2023-07-21 16:11:50 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/More/TopUp/MobileTopUp/BottomSheetVC.swift
+++ UberPath/UberPath/Controller/Home/Home/More/TopUp/MobileTopUp/BottomSheetVC.swift
@@ -13,60 +13,59 @@ class BottomSheetVC: UIViewController {
     @IBOutlet weak var lbl2: UILabel!
     @IBOutlet weak var amountTxtFld: UITextField!
     @IBOutlet weak var slide: UISlider!
-   
-    var model : MobileTopUpTableViewModel?
+    
+    @IBOutlet var moneyButtons: [UIButton]! // Outlet collection for the money buttons
+    
+    var model: MobileTopUpTableViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         update()
     }
     
     @IBAction func minusBtnPressed(_ sender: UIButton) {
-        
+        // Handle the minus button press here if needed
     }
     
     @IBAction func plusBtnPressed(_ sender: UIButton) {
+        // Handle the plus button press here if needed
     }
     
     @IBAction func moneyBtnPressed(_ sender: UIButton) {
-        if sender.tag == 1 {
-            amountTxtFld.text = String(5)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
-        } else if sender.tag == 2 {
-            amountTxtFld.text = String(10)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
-        } else if sender.tag == 3 {
-            amountTxtFld.text = String(15)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
-        } else if sender.tag == 4 {
-            amountTxtFld.text = String(20)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
-        } else if sender.tag == 5 {
-            amountTxtFld.text = String(50)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
-        } else if sender.tag == 6 {
-            amountTxtFld.text = String(100)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
-        } else if sender.tag == 7 {
-            amountTxtFld.text = String(200)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
-        } else if sender.tag == 8 {
-            amountTxtFld.text = String(500)
-            sender.backgroundColor = UIColor(named: "LaunchscreenColor")
-            sender.tintColor = .white
+        let amounts = [5, 10, 15, 20, 50, 100, 200, 500]
+        
+        if let index = moneyButtons.firstIndex(of: sender) {
+            if index < amounts.count {
+                let amount = amounts[index]
+                amountTxtFld.text = String(amount)
+            }
+            
+            // Update the appearance of the money buttons
+            updateMoneyButtonsAppearance(selectedButton: sender)
         }
     }
-  
-    func update(){
-        guard let data = model else {return}
+    
+    func update() {
+        guard let data = model else { return }
         img.image = UIImage(named: data.img)
         lbl1.text = data.lbl1
         lbl2.text = data.lbl2
+        
+        // Set the default amount value in the text field
+        amountTxtFld.text = "0"
+    }
+    
+    func updateMoneyButtonsAppearance(selectedButton: UIButton) {
+        for button in moneyButtons {
+            if button == selectedButton {
+                // Set the selected button appearance
+                button.backgroundColor = UIColor(named: "LaunchscreenColor")
+                button.tintColor = .white
+            } else {
+                // Set the normal appearance for other buttons
+                button.backgroundColor = .systemGray6
+                button.tintColor = UIColor(named: "GlobalButtonColor")
+            }
+        }
     }
 }
