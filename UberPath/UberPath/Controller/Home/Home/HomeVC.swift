6ee5b25bac319f23d2b89a0c57317bb860686af5
Darshan <>
2023-07-24 17:17:13 +0530

    new

--- UberPath/UberPath/Controller/Home/Home/HomeVC.swift
+++ UberPath/UberPath/Controller/Home/Home/HomeVC.swift
@@ -19,12 +19,28 @@ class HomeVC: UIViewController {
     }
     
     @IBAction func logOutBtnPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Logout", message: "Are you sure you want to logout?", preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            // Perform the logout action here
+            self.performLogout()
+        }
+        alertController.addAction(yesAction)
+        
+        let noAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        alertController.addAction(noAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func performLogout() {
+        // Perform the logout logic here, for example:
         viewModel.logout()
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let destinationVC = storyboard.instantiateViewController(withIdentifier: "OnboardingVC") as! OnboardingVC
         navigationController?.pushViewController(destinationVC, animated: true)
-        
     }
+
     
     @IBAction func depositBtnPressed(_ sender: Any) {
         print("Deposit")
